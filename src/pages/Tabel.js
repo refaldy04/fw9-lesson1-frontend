6eda3c1087591e33ee8413a581b95ae643cc9b4a
refaldy04 <refaldybanggana@gmail.com>
2022-09-27 23:57:12 +0700

    create table and integrate

--- src/pages/Tabel.js
+++ src/pages/Tabel.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Layout from '../components/Main';
+import axios from 'axios';
 
 function Tabel() {
-  return <div>Tabel</div>;
+  const [dataTable, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const data = await axios.get('http://localhost:3314/contactUs');
+      console.log(data);
+      setData(data.data);
+      // ...
+    }
+    fetchData();
+  }, []);
+  return (
+    <Layout>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Message</th>
+          </tr>
+          {dataTable.map((data) => (
+            <>
+              <tr>
+                <th>{data.name}</th>
+                <th>{data.email}</th>
+                <th>{data.message}</th>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
 
 export default Tabel;
