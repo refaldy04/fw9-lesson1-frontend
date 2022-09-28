294c3b88bfe9bc2a813974c887bf11411eebb344
refaldy04 <refaldybanggana@gmail.com>
2022-09-29 01:30:44 +0700

    add limit and sort

--- src/pages/Tabel.js
+++ src/pages/Tabel.js
@@ -4,17 +4,20 @@ import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function Tabel() {
   const [dataTable, setData] = useState([]);
   const [pageInfo, setPageInfo] = useState(null);
+  const [keyword, setKeyword] = useState('');
 
   const navigate = useNavigate();
 
-  const getData = (limit = 5, page = 1) => {
+  const getData = (limit = 5, page = 1, search = keyword, sort = 'name') => {
     limit = parseInt(limit);
     page = parseInt(page);
-    const query = new URLSearchParams({ limit, page }).toString();
+    const query = new URLSearchParams({ limit, page, search, sort }).toString();
     axios.get(`http://localhost:3314/contactUs?` + query).then(({ data }) => {
       setData(data.result);
       setPageInfo(data.pageInfo);
@@ -33,11 +36,32 @@ function Tabel() {
     getData(pageInfo.limit, pageInfo.prevPage);
   };
 
+  const onSearch = (e) => {
+    e.preventDefault();
+    getData(pageInfo.limit, 1, keyword);
+  };
+
   return (
-    <Layout>
+    <Layout className="ps-5">
       <Button variant="dark" type="submit" className="mt-5 ms-4" onClick={() => navigate('/')}>
         Back
       </Button>
+
+      <div className="d-flex justify-content-start gap-5">
+        <Form onSubmit={onSearch}>
+          <InputGroup className="my-3 ms-4 me-5">
+            <Button variant="dark" id="button-addon1" type="submit">
+              Search
+            </Button>
+            <Form.Control aria-label="Example text with button addon" aria-describedby="basic-addon1" onChange={(e) => setKeyword(e.target.value)} />
+          </InputGroup>
+        </Form>
+        <select aria-label="Default select example" onChange={(e) => getData(pageInfo.limit, 1, '', e.target.value)}>
+          <option>Sort By</option>
+          <option value={'name'}>Name</option>
+          <option value={'email'}>Email</option>
+        </select>
+      </div>
 
       <Table striped bordered hover className="mb-5 mt-3">
         <thead>
@@ -72,7 +96,7 @@ function Tabel() {
           <option value={2}>2</option>
           <option value={3}>3</option>
           <option value={4}>4</option>
-          <option value={5} selected>
+          <option value={5} defaultValue>
             5
           </option>
         </select>
