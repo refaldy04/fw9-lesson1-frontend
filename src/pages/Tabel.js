0f3f8215346292d332099afb4a41374fe105f765
refaldy04 <refaldybanggana@gmail.com>
2022-09-28 15:39:59 +0700

    create limit

--- src/pages/Tabel.js
+++ src/pages/Tabel.js
@@ -67,6 +67,15 @@ function Tabel() {
         <Button variant="dark" disabled={pageInfo?.nextPage === null} onClick={getNextPage}>
           Next
         </Button>
+        <select onChange={(e) => getData(e.target.value)}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5} selected>
+            5
+          </option>
+        </select>
       </div>
     </Layout>
   );
