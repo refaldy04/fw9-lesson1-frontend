87937324a682e5947e0ab094ee1404c09317bc85
refaldy04 <refaldybanggana@gmail.com>
2022-09-29 16:50:02 +0700

    fix

--- src/pages/Tabel.js
+++ src/pages/Tabel.js
@@ -156,9 +156,7 @@ function Tabel() {
           <option value={2}>2</option>
           <option value={3}>3</option>
           <option value={4}>4</option>
-          <option value={5} defaultValue>
-            5
-          </option>
+          <option value={5}>5</option>
         </select>
       </div>
     </Layout>
